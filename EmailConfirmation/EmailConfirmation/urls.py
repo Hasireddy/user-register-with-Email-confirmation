11d3ec048b1d1503a2f8ffb83f3819e883f9474f
Hasireddy <malikireddy.uma@gmail.com>
2024-11-05 11:17:00 +0100

    App urls included in the project urls

--- EmailConfirmation/EmailConfirmation/urls.py
+++ EmailConfirmation/EmailConfirmation/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,render
+from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("user/",render("EmailApp.urls"))
+    path("user/",include("EmailApp.urls"))
 ]
