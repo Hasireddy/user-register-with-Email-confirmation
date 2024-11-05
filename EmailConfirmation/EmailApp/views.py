07fc5b243d967a6374f966be156df3af146f3215
Hasireddy <malikireddy.uma@gmail.com>
2024-11-05 11:50:19 +0100

    user register view

--- EmailConfirmation/EmailApp/views.py
+++ EmailConfirmation/EmailApp/views.py
@@ -1,5 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
 
-from .models import User
+from .forms import UserRegisterForm
 
 # Create your views here.
+
+def user_register(request):
+    if request.method == "POST":
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponse("user registered")
+    else:       
+        form = UserRegisterForm()
+    return render(request,"register.html",{"register_form":form})
+        
+
+
